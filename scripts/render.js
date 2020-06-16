039c4fe270287f3ec49a1308d633c9d67e88c138
Vladislav Korenik <vladislavkorenik@gmail.com>
2020-06-16 21:24:08 +0300

    changed servics-worker.js

--- scripts/render.js
+++ scripts/render.js
@@ -1,44 +1,28 @@
-let hasOnScreen = false;
 let deferredPrompt;
+const addBtn = document.getElementById("addToHomeScreen");
+addBtn.style.display = "none";
 
-const isIos = () => {
-  const userAgent = window.navigator.userAgent.toLowerCase();
-  return /iphone|ipad|ipod/.test(userAgent);
-};
-// Detects if device is in standalone mode
-const isInStandaloneMode = () =>
-  "standalone" in window.navigator && window.navigator.standalone;
+window.addEventListener("beforeinstallprompt", (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  addBtn.style.display = "block";
 
-// Checks if should display install popup notification:
-if (isInStandaloneMode()) {
-  hasOnScreen = true;
-}
-
-$(document).ready(() => {
-  if (hasOnScreen) {
-    $("#addToHomeScreen").css("display", "none");
-  }
-  window.addEventListener("beforeinstallprompt", (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    // Update UI to notify the user they can add to home screen
-
-    $("#addToHomeScreen").click(function (e) {
-      // hide our user interface that shows our A2HS button
-      $(this).css("display", "none");
-      // Show the prompt
-      deferredPrompt.prompt();
-      // Wait for the user to respond to the prompt
-      deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === "accepted") {
-          console.log("User accepted the A2HS prompt");
-        } else {
-          console.log("User dismissed the A2HS prompt");
-        }
-        deferredPrompt = null;
-      });
+  addBtn.addEventListener("click", (e) => {
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted prompt");
+        // hide our user interface that shows our A2HS button
+        addBtn.style.display = "none";
+      } else {
+        console.log("User dismissed prompt");
+      }
+      deferredPrompt = null;
     });
   });
 });
--- service-worker.js
+++ service-worker.js
@@ -3,10 +3,11 @@ const RUNTIME = "runtime";
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-  "index.html",
+  "icons/logo.svg",
+  /*"index.html",
   "scripts/jquery.js",
   "scripts/render.js",
-  "styles/style.css",
+  "styles/style.css",*/
 ];
 
 // The install handler takes care of precaching the resources we always need.
