571e118953fad7ecd14966f72d8a960ebecbdfe6
Vladislav Korenik <vladislavkorenik@gmail.com>
2020-06-17 16:01:55 +0300

    service-worker off

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,28 @@
+// Set this to true for production
+const doCache = false;
+
 const PRECACHE = "precache-v1";
 const RUNTIME = "runtime";
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-  "images/logo.svg",
-  /*"index.html",
+  "index.html",
   "scripts/jquery.js",
   "scripts/render.js",
-  "styles/style.css",*/
+  "scripts/loginFormHandler.js",
+  "styles/style.css",
 ];
 
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches
-      .open(PRECACHE)
-      .then((cache) => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
-  );
+  if (doCache) {
+    event.waitUntil(
+      caches
+        .open(PRECACHE)
+        .then((cache) => cache.addAll(PRECACHE_URLS))
+        .then(self.skipWaiting())
+    );
+  }
 });
 
 // The activate handler takes care of cleaning up old caches.
@@ -46,23 +51,25 @@ self.addEventListener("activate", (event) => {
 // If no response is found, it populates the runtime cache with the response
 // from the network before returning it to the page.
 self.addEventListener("fetch", (event) => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
+  if (doCache) {
+    // Skip cross-origin requests, like those for Google Analytics.
+    if (event.request.url.startsWith(self.location.origin)) {
+      event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+          if (cachedResponse) {
+            return cachedResponse;
+          }
 
-        return caches.open(RUNTIME).then((cache) => {
-          return fetch(event.request).then((response) => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
+          return caches.open(RUNTIME).then((cache) => {
+            return fetch(event.request).then((response) => {
+              // Put a copy of the response in the runtime cache.
+              return cache.put(event.request, response.clone()).then(() => {
+                return response;
+              });
             });
           });
-        });
-      })
-    );
+        })
+      );
+    }
   }
 });
