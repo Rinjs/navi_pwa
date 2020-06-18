88416b9edd3a05645dd7934117b160720025889d
Vladislav Korenik <vladislavkorenik@gmail.com>
2020-06-18 15:54:04 +0300

    test bg

--- scripts/render.js
+++ scripts/render.js
@@ -1,12 +1,113 @@
 let loginPage = $("#loginPage");
-let authorized = false;
+let homePage = $("#homePage");
+let authorized = true;
+let userLogin = "user";
 
 const generateHomePage = () => {
+  const autoList = [
+    {
+      name: "Polo 1",
+      address:
+        "address, address, address, address, address, address, address, address,",
+    },
+    {
+      name: "Polo 2",
+      address:
+        "address, address, address, address, address, address, address, address,",
+    },
+    {
+      name: "Polo 3",
+      address:
+        "address, address, address, address, address, address, address, address,",
+    },
+    {
+      name: "SKODA",
+      address:
+        "address, address, address, address, address, address, address, address,",
+    },
+    {
+      name: "SKODA 1",
+      address:
+        "address, address, address, address, address, address, address, address,",
+    },
+    {
+      name: "SKODA 2",
+      address:
+        "address, address, address, address, address, address, address, address,",
+    },
+    {
+      name: "SKODA 3",
+      address:
+        "address, address, address, address, address, address, address, address,",
+    },
+    {
+      name: "Polo 10",
+      address:
+        "address, address, address, address, address, address, address, address,",
+    },
+    {
+      name: "Auto 1",
+      address:
+        "address, address, address, address, address, address, address, address,",
+    },
+    {
+      name: "Auto 1",
+      address:
+        "address, address, address, address, address, address, address, address,",
+    },
+    {
+      name: "Auto 1",
+      address:
+        "address, address, address, address, address, address, address, address,",
+    },
+    {
+      name: "Auto 1",
+      address:
+        "address, address, address, address, address, address, address, address,",
+    },
+    {
+      name: "Auto 1",
+      address:
+        "address, address, address, address, address, address, address, address,",
+    },
+    {
+      name: "Auto 1",
+      address:
+        "address, address, address, address, address, address, address, address,",
+    },
+    {
+      name: "Polo 1",
+      address:
+        "address, address, address, address, address, address, address, address,",
+    },
+    {
+      name: "Polo 1",
+      address:
+        "address, address, address, address, address, address, address, address,",
+    },
+    {
+      name: "Polo 1",
+      address:
+        "address, address, address, address, address, address, address, address,",
+    },
+  ];
+
   $("main").append(
     "<div class='home-page' id='homePage'>" +
       "<div class='nav-menu' id='navMenu'>" +
       "<div class='nav-menu__toggle'><span></span><span></span><span></span></div>" +
       "<p class='nav-menu__text' >Объекты</p>" +
+      "<div class='nav-menu__list-container' id='navMenuList'>" +
+      `<p class='nav-menu__list-text'>${userLogin}</p>` +
+      "<ul class='nav-menu__list'>" +
+      "<li class='nav-menu__item'>" +
+      "<p class='nav-menu__item-text' id='homeAutoList'>Автомобили</p>" +
+      "</li>" +
+      "<li class='nav-menu__item'>" +
+      "<p class='nav-menu__item-text' id='backToLoginPage'>Выход</p>" +
+      "</li>" +
+      "</ul>" +
+      "</div>" +
       "</div>" +
       "<div class='auto-list__container'>" +
       "<form class='auto-list__search-form' id='searchForm'>" +
@@ -17,10 +118,18 @@ const generateHomePage = () => {
       "<i class='auto-list__search-icon'></i>" +
       "</div>" +
       "</form>" +
-      "<ul class='auto-list'>" +
+      "<ul class='auto-list' id='autoList'>" +
       "</ul></div>" +
       "</div>"
   );
+
+  $(".nav-menu__toggle").click(function () {
+    $("#navMenuList").addClass("show");
+    $("#homePage").addClass("tinting")
+  });
+  $("#homeAutoList").click(function () {
+    $("#navMenuList").removeClass("show");
+  });
 
   $("#clearSearchFormInput")
     .addClass("hidden")
@@ -38,6 +147,12 @@ const generateHomePage = () => {
       $("#clearSearchFormInput").addClass("hidden");
       $(".auto-list__form-label").removeClass("hidden");
     }
+  });
+
+  autoList.forEach((auto) => {
+    $("#autoList").prepend(
+      `<li class="auto-list__item"><h1 class="auto-name">${auto.name}</h1><p class="auto-address">${auto.address}</p><i id="carStatistics"></i></li>`
+    );
   });
 };
 if (authorized) {
